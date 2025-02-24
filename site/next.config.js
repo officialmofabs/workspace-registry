4134eddde59cc186e92c38cf750bfb1211284bbb
Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>
2025-02-24 02:26:27 +0000

    Update next.config.js
    Signed-off-by: Moses Fabiyi <90117777+officialmofabs@users.noreply.github.com>

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'officialmofabs Registry',
+    description: 'The official store for my workspace builds.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'hhttps://github.com/officialmofabs/workspaces-registry',
+    contactUrl: 'https://github.com/officialmofabs/support',
   },
   reactStrictMode: true,
   swcMinify: true,
